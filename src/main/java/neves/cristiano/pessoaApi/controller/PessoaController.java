bb461f2b820258d83129291582534d77b989acce
milrem <cris.a.neves@gmail.com>
2023-01-20 22:17:50 -0300

    Criar controler base para API de Pessoa

---
+++ src/main/java/neves/cristiano/pessoaApi/controller/PessoaController.java
@@ -0,0 +1,9 @@
+package neves.cristiano.pessoaApi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/pessoa")
+public class PessoaController {
+}
---
+++ src/test/java/neves/cristiano/pessoaApi/PessoaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package neves.cristiano.pessoaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
