d17d0fafa7bf206b31047fcf1f30ef97be61b13e
milrem <cris.a.neves@gmail.com>
2023-01-22 15:00:13 -0300

    Implementado o add no PessoaController retornando Http Status 201

--- src/main/java/neves/cristiano/pessoaApi/controller/PessoaController.java
+++ src/main/java/neves/cristiano/pessoaApi/controller/PessoaController.java
@@ -1,9 +1,31 @@
 package neves.cristiano.pessoaApi.controller;
 
+import neves.cristiano.pessoaApi.dto.PessoaDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/pessoa")
 public class PessoaController {
+    @PostMapping
+    public ResponseEntity<Void> add(@RequestBody PessoaDTO pessoa) {
+        if (pessoa == null || pessoa.getCpf() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Não foi informada uma pessoa válida");
+        }
+        String clearCpf = pessoa.getCpf().replaceAll("[\\.-]", "");
+        if (clearCpf.isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Não foi informada uma pessoa com CPF válido");
+        }
+        if (pessoa.getNome() == null || pessoa.getNome().isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Não foi informada uma pessoa com Nome válido");
+        }
+        return ResponseEntity.created(URI.create("/api/pessoa/" + clearCpf)).build();
+    }
 }
